573221ab5638c6d70826022efba90ae273cec09a
Carlos Rios <hello@carlosrios.co.uk>
2016-11-15 10:17:49 +0000

    removes output files from root

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/app.module.js',
     output: {
-        path: './',
+        path: './dist/',
         filename: 'app.js'
     },
     plugins: [
